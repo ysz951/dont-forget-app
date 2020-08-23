cda5f95c2781cedd2bdabfaf953fb130ad7b2542
ysz951 <shengyang_zhou@baylor.edu>
2020-08-23 03:55:19 -0500

    delete useless code

--- src/RegistrationForm/RegistrationForm.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import RegistrationForm from './RegistrationForm';
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<RegistrationForm />, div)
-  ReactDOM.unmountComponentAtNode(div)
-});
