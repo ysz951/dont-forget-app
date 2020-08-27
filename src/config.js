74f75ae82f2cd15ccc2c100d47d8b5c155b6de62
ysz951 <shengyang_zhou@baylor.edu>
2020-08-27 01:26:42 -0500

    change endpoint

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-    API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT || "http://localhost:8000/api",
-    TOKEN_KEY: process.env.REACT_APP_TOKEN_KEY || 'dont-forget-auth-token',
+    API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT || "http://damp-atoll-19992.herokuapp.com/api",
+    TOKEN_KEY: process.env.REACT_APP_TOKEN_KEY,
 };
