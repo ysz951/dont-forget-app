228262c8fe031cbd997332a239ee70d13ec24f91
ysz951 <shengyang_zhou@baylor.edu>
2020-08-27 01:42:15 -0500

    change http to https

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-    API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT || "http://damp-atoll-19992.herokuapp.com/api",
+    API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT || "https://damp-atoll-19992.herokuapp.com/api",
     TOKEN_KEY: process.env.REACT_APP_TOKEN_KEY,
 };
